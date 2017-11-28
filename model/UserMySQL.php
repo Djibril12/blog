588fbc6238972c68e72669df1038212f643351b1
bao <bao@bao.pc>
2017-11-29 00:37:13 +0100

    authentification utilisateur après inscription

--- model/UserMySQL.php
+++ model/UserMySQL.php
@@ -11,38 +11,59 @@ class UserMySQL implements UserHandler {
     // cette fonction permet d'authentifier l'utilisateur
     public function authenticate($username, $password) {
         // recherche du username dans la bdd
+        $sql = 'SELECT * FROM user WHERE username = :name';
+
+        $stmt = $$this->pdo->prepare($sql);
+
+        $result = $stmt->execute(array(
+            'name' => $username
+        ));
+
+        if ($result) {
+            // recupération de l'utilisateur
+            $userDB = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            // verification du mot de passe avec la fonction password_verify 
+            if ($userDB && password_verify($password, $userDB['password'])) {
+                // création d'un nouvel utilisateur
+                $user = new User();
+                
+                //
+                $user->setId($userDB['id']);
+                $user->setName($userDB['name']);
+                $user->setEmail($userDB['email']);
+                $user->setCreatedAt(new DateTime($userDB['created_at']));
+                
+                // renvoi un objet user si l'authentification a reussie
+                return $user;
+            }
+        }
         
-        // verification du mot de passe avec la fonction password_verify 
+        // renvoi false si l'authentification a échoué
+        return false;
     }
 
     public function add(User $user) {
-//        echo 'add function <br>';
-//        debug_value($user);
-//
-//        $res = $this->pdo->exec('INSERT INTO user (id, username, password,  email, created_at) '
-//                . 'VALUES(null, \'alpha\', \'alpha@yengue.com\', \'password123\', \'2017-08-31 00:00:00\')');
-//
-//        echo $res . 'L\'utilisateur a bien été ajouté !';
-//        die();
-//        
+
         // hash du mot de passe avec l'algo BCRYPT
         $password = password_hash($user->getPassword(), PASSWORD_BCRYPT);
-        
+
         // formatage de la date
         $date = $user->getCreatedAt()->format('Y-m-d h:i:s');
         // création de la requête
-        $sql = 'INSERT INTO user VALUES(null, :name, :pass, :email, :date)';       
+        $sql = 'INSERT INTO user VALUES(null, :name, :pass, :email, :date)';
         //echo $sql;
- 
+
         $stmt = $this->pdo->prepare($sql);
-        $res =  $stmt->execute(array(
+
+        $res = $stmt->execute(array(
             'name' => $user->getUsername(),
             'pass' => $password,
             'email' => $user->getEmail(),
             'date' => $date,
         ));
-        
-        echo $res;
+
+        return $res;
     }
 
     public function all() {
--- web/inscription.php
+++ web/inscription.php
@@ -1,7 +1,8 @@
 <?php
+
 // register.php
- include dirname(__DIR__). DIRECTORY_SEPARATOR . 'init.php';
- 
+include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'init.php';
+
 $email = isset($_POST['email']) ? $_POST['email'] : null;
 $username = isset($_POST['username']) ? $_POST['username'] : null;
 $password = isset($_POST['password']) ? $_POST['password'] : null;
@@ -9,14 +10,11 @@ $password = isset($_POST['password']) ? $_POST['password'] : null;
 // tableau des messsages d'erreurs
 $errors = [];
 
-
-
 // test si le formulaire a été soumis
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
-    debug_value($_POST);
-    
-    
+
+    //debug_value($_POST);
+
     if (!Validator::validUsername($username)) {
         $errors[] = 'Username incorrect';
     }
@@ -26,47 +24,47 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!Validator::validPassword($password)) {
         $errors[] = 'Mot de passe incorrect (entre 6 & 12)';
     }
-    
 
-    
     if (empty($errors)) {
         // création d'un user
         $user = new User();
+        
+        // insertion des données dans un tableau
         $donnees = array(
             $username,
             $email,
             $password,
             new DateTime()
         );
-    
-        
-        
+
         $user->setUsername($username);
         $user->setEmail($email);
         $user->setPassword($password);
         $user->setCreatedAt(new DateTime());
-       
+
         //echo $user;
         //debug_value($user);
-        
+
         $userModel = new UserMySQL($db);
         //debug_value($userModel);
         //die();
         $userModel->add($user);
         if ($userModel->add($user)) {
-          
+
+            // par défaut on connecte l'utilisateur avec la fonction authenticate
             $user = $userModel->authenticate($username, $password);
             if ($user) {
+
                 $_SESSION['user'] = $user;
                 header('Location: index.php');
             } else {
                 $errors[] = 'Impossible de vous connecter';
             }
         } else {
-           echo "Une erreur s'est produite";
+            echo "Une erreur s'est produite";
         }
     }
 }
 
-include  VIEWS_INS . DIRECTORY_SEPARATOR . 'inscription.phtml' ; 
+include VIEWS_INS . DIRECTORY_SEPARATOR . 'inscription.phtml';
 ?>
